5b0af9ff0883e4019ca36e8d2bf8b442f4e24396
Goodness Igunma <Goodnessigunma1@gmail.com>
2023-07-08 23:18:32 +0100

    Fake data added

---
+++ FakeData.js
@@ -0,0 +1,72 @@
+export const UserList = [
+  {
+    id: 1,
+    name: 'John Doe',
+    username: 'johndoe',
+    age: 25,
+    nationality: 'USA'
+  },
+  {
+    id: 2,
+    name: 'Jane Smith',
+    username: 'janesmith',
+    age: 30,
+    nationality: 'Canada'
+  },
+  {
+    id: 3,
+    name: 'Alice Johnson',
+    username: 'alicejohnson',
+    age: 28,
+    nationality: 'Australia'
+  },
+  {
+    id: 4,
+    name: 'Bob Williams',
+    username: 'bobwilliams',
+    age: 32,
+    nationality: 'UK'
+  },
+  {
+    id: 5,
+    name: 'Emily Davis',
+    username: 'emilydavis',
+    age: 27,
+    nationality: 'Germany'
+  },
+  {
+    id: 6,
+    name: 'Michael Brown',
+    username: 'michaelbrown',
+    age: 35,
+    nationality: 'USA'
+  },
+  {
+    id: 7,
+    name: 'Sophia Martinez',
+    username: 'sophiamartinez',
+    age: 29,
+    nationality: 'Spain'
+  },
+  {
+    id: 8,
+    name: 'Daniel Johnson',
+    username: 'danieljohnson',
+    age: 31,
+    nationality: 'Canada'
+  },
+  {
+    id: 9,
+    name: 'Olivia Wilson',
+    username: 'oliviawilson',
+    age: 26,
+    nationality: 'Australia'
+  },
+  {
+    id: 10,
+    name: 'James Anderson',
+    username: 'jamesanderson',
+    age: 33,
+    nationality: 'UK'
+  }
+];
